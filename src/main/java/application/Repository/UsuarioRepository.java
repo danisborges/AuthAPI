5e61e5c6969f94e41a149ff625edfd03858ab8ea
danis borges <borgesdanielli81@gmail.com>
2025-04-22 17:53:52 +0000

    commit incial

---
+++ src/main/java/application/Repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package application.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import application.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Usuario findByNomeDeUsuario(String nomeDeUsuario);  
+}
